916ba9576c83f5eaf28f345625965cdc0d90c0a6
rpinor <rpinor@unjbg.edu.pe>
2024-06-24 05:28:28 -0500

    Actualizando ejercicio1.cpp: Agrega librerias y mensajes de salida

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 int main (){
     float op1, op2, r;
     char op;
+    cout<<"Ingrese los dos operandos"<<endl;
     cin>>op1;
     cin>>op2;
+    cout<<"Ingrese el operador"<<endl;
     cin>>op;
+    system("cls");
     switch (op){
         case '+':
             r=op1+op2;
+            cout<<r<<endl;
         break;
         case '-':
             r=op1-op2;
+            cout<<r<<endl;
         break;
         case '*':
             r=op1*op2;
+            cout<<r<<endl;
         break;
         case '/':
-            r=op1/op2;
+            if(op2!=0){
+				r=op1/op2;
+				cout<<r<<endl;
+			} else {
+				cout<<"No se puede dividir";
+			}
         break;
     }
-    cout<<r;
+    system("pause");
     return 0;
 }
