c4914e9bdf294922328949d665288b87cf09e06b
rpinor <rpinor@unjbg.edu.pe>
2024-06-24 05:39:41 -0500

    Actualizando ejercicio2.cpp: Agrega libreria, validaciones y mensajes de salida

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 int main (){
     int n1, n2, c, i;
+    cout<<"Ingrese dos numeros:"<<endl;
     cin>>n1;
     cin>>n2;
     c=0;
     i=1;
-    while((i<=n1)&&(i<=n2)){
+    system("cls");
+    if((n1>0)&&(n2>0)){
+        while((i<=n1)&&(i<=n2)){
         if((n1%i==0)&&(n2%i==0)){
             c=c+1;
         }
         i=i+1;
     }
     if(c>1){
-        cout<<"No son PESI";
+        cout<<"No son PESI"<<endl;
     } else {
-        cout<<"Son PESI";
+        cout<<"Son PESI"<<endl;
     }
+    } else {
+        cout<<"Opcion(es) no valida(s)"<<endl;
+    }
+    system("pause");
     return 0;
 }
